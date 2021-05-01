5e0bdd50f0e2359e5948ee441598f281df5b92c6
Adaypon <75831434+Adaypon@users.noreply.github.com>
2021-05-01 17:39:40 +0300

    Included MyVector.h to main.cpp

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,18 +1,8 @@
-#include "Vector.h"
+#include "MyVector.h"
 
 int main() {
-	Vector a;
-	for (size_t i = 1; i <= 10; ++i) {
-		a.pushBack(i);
-	}
-	a.pushBack(9);
-
-	for (size_t i = 0; i < a.size(); ++i) {
-		std::cout << a[i] << ' ';
-	}
-	std::cout << std::endl;
-
-	std::cout << a.find(9) << std::endl;
+	MyVector a(5);
+	
 
 	return 0;
 }
